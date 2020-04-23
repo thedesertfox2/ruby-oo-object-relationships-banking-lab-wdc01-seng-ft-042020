33896f948cf498e6bf15efc8e487b465091d271c
saimaja <saimaakhtar@gmail.com>
2020-04-23 17:36:02 -0400

    saima's edits

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
+require_relative '../lib/bank_account'
+
 class Transfer
   # your code here
+
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    
+  end
+
+
+
 end

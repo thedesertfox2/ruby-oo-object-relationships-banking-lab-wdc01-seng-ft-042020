731c5ec6abebe05d83fbd177831afc276170e4c8
Alexander_Gautherin <alexander.gautherin@uconn.edu>
2020-04-23 15:48:48 -0400

    Bank_Account Complete

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status
+    @@all = []
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
+
+    def deposit(money)
+        @balance += money
+    end
+
+    def display_balance
+        "Your balance is $#{balance}."
+    end
+
+    def valid?
+        self.balance > 0 && self.status == "open"
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
+
+
